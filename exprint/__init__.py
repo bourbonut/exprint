28e6eacbdcaf7d0eede94ed70eb679b25907f499
bourbonut <ben.bourbon06@gmail.com>
2026-02-03 13:29:02 +0100

    Add `__version__` in `exprint/__init__.py`

--- exprint/__init__.py
+++ exprint/__init__.py
@@ -1,6 +1,10 @@
+import importlib.metadata
+
 from .colors import ANSIColors
 from .exprint import dispatch_generic, dispatch_obj, dispatch_repr, exprint
 from .formatter import Format, Formatter
+
+__version__ = importlib.metadata.version(__package__)  # type: ignore
 
 __all__ = [
     "ANSIColors",
